12f751f65518b82a216f49e6d7fe11e6215b51ac
upnt <upnt.github@gmail.com>
2022-07-04 16:08:18 +0900

    add git branch name

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -9,31 +9,33 @@ Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
 function prompt {
+    $path = (Get-Location).ToString().Replace((Convert-Path ~), "~")
+    $branch = (git branch --show-current)
+
     ## first line
     Write-Host ""
 
     ## second line
-    $path = (Get-Location).ToString().Replace((Convert-Path ~), "~")
-    Write-Host "`u{256d}" -foregroundcolor Blue -nonewline 
-    Write-Host "`u{2500}`u{2500}" -foregroundcolor Blue -nonewline 
-    Write-Host "`u{E0B6}" -foregroundcolor Blue -nonewline 
+    Write-Host "`u{256d}`u{2500}`u{2500}`u{E0B6}" -ForeGroundColor Blue -nonewline 
+    Write-Host " $env:USERNAME " -ForeGroundColor White -BackGroundColor Blue -NoNewLine
+    Write-Host "`u{E0B0}" -ForeGroundColor Blue -BackGroundColor DarkGray -nonewline
 
-    Write-Host " " -backgroundcolor Blue -nonewline
-    Write-Host $env:USERNAME -backgroundcolor Blue -nonewline
-    Write-Host " " -backgroundcolor Blue -nonewline
-    Write-Host "`u{E0B0}" -foregroundcolor Blue -backgroundcolor DarkGray -nonewline
+    Write-Host " $path " -BackGroundColor DarkGray -NoNewLine
 
-    Write-Host " " -backgroundcolor DarkGray -nonewline
-    Write-Host $path -backgroundcolor DarkGray -nonewline
-    Write-Host " " -backgroundcolor DarkGray -nonewline
-    Write-Host "`u{E0B0}" -foregroundcolor DarkGray -nonewline
+    if ($branch -ne $null) {
+        Write-Host "`u{E0B0}" -ForeGroundColor DarkGray -BackGroundColor DarkGreen -nonewline
+
+        Write-Host " `u{2E19} $branch " -ForeGroundColor Black -BackGroundColor DarkGreen -NoNewLine
+        Write-Host "`u{E0B0}" -ForeGroundColor DarkGreen -nonewline
+    } else {
+        Write-Host "`u{E0B0}" -ForeGroundColor DarkGray -nonewline
+    }
 
     Write-Host ""
 
     # third line
-    Write-Host "`u{2570}" -foregroundcolor Cyan -nonewline 
-    Write-Host "`u{2500}" -foregroundcolor Cyan -nonewline 
-    Write-Host "`u{232A}" -foregroundcolor White -nonewline 
+    Write-Host "`u{2570}`u{2500}" -ForeGroundColor Cyan -nonewline 
+    Write-Host "`u{232A}" -ForeGroundColor White -nonewline 
 
     return " "
 }
