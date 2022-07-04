0539d1bd187fc3760750179fc917449264e69a02
upnt <upnt.github@gmail.com>
2022-07-04 14:46:21 +0900

    make terminal beautiful

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -9,15 +9,35 @@ Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
 function prompt {
-    $line = ""
-    Write-Host $line
+    ## first line
+    Write-Host ""
 
-    $path = Get-Location
-    $line = "`u{256d}" + "`u{2500}`u{2500}" + " " + $path
-    Write-Host $line
+    ## second line
+    $path = (Get-Location).ToString().Replace((Convert-Path ~), "~")
+    Write-Host "`u{256d}" -foregroundcolor Blue -nonewline 
+    Write-Host "`u{2500}`u{2500}" -foregroundcolor Blue -nonewline 
+    Write-Host "`u{E0B6}" -foregroundcolor Blue -nonewline 
 
-    return "`u{2570}" + "`u{2500}" + "`u{232A}"
+    Write-Host " " -backgroundcolor Blue -nonewline
+    Write-Host $env:USERNAME -backgroundcolor Blue -nonewline
+    Write-Host " " -backgroundcolor Blue -nonewline
+    Write-Host "`u{E0B0}" -foregroundcolor Blue -backgroundcolor DarkGray -nonewline
+
+    Write-Host " " -backgroundcolor DarkGray -nonewline
+    Write-Host $path -backgroundcolor DarkGray -nonewline
+    Write-Host " " -backgroundcolor DarkGray -nonewline
+    Write-Host "`u{E0B0}" -foregroundcolor DarkGray -nonewline
+
+    Write-Host ""
+
+    # third line
+    Write-Host "`u{2570}" -foregroundcolor Cyan -nonewline 
+    Write-Host "`u{2500}" -foregroundcolor Cyan -nonewline 
+    Write-Host "`u{232A}" -foregroundcolor White -nonewline 
+
+    return " "
 }
+
 
 
 $AutoloadDir = (Get-Item $PROFILE).DirectoryName + "\Autoload"
