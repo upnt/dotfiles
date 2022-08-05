91ae4f558408208a98705a61921db227486fe048
upnt <upnt.github@gmail.com>
2022-08-06 02:16:08 +0900

    add boxstarter script

---
+++ rc/boxstarter/boxstarter.ps1
@@ -0,0 +1,42 @@
+Install-WindowsUpdate -AcceptEula
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions
+wsl --install -d ubuntu
+
+cinst wget
+cinst git
+cinst findutils
+cinst busybox
+cinst grep
+cinst gh
+cinst fzf
+cinst mingw
+
+cinst font-hackgen
+cinst font-hackgen-nerd
+cinst cica
+
+cinst powershell-core
+cinst wezterm
+cinst vscode
+cinst devtoys
+cinst drawio
+cinst docker-desktop
+
+cinst logitech-camera-settings
+cinst lghub
+cinst wacom-drivers
+
+cinst steam
+cinst epicgameslauncher
+cinst obs-studio
+cinst foobar2000
+cinst mp3tag
+
+cinst firefox
+cinst googlechrome
+
+cinst line
+cinst discord
+cinst zoom
+cinst slack
+cinst microsoft-teams
