ff9a12e06dff41f02a7fe885cbaaa2f1a54183ad
upnt <upnt.github@gmail.com>
2022-08-16 17:34:15 +0900

    change font name

--- rc/wezterm/.wezterm.lua
+++ rc/wezterm/.wezterm.lua
@@ -4,7 +4,7 @@ return {
     color_scheme = "iceberg-dark",
     font_size = 14.0,
     font = wezterm.font({
-        family = 'HackGen35Nerd Console',
+        family = 'HackGen35 Console NFJ',
         harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
     }),
     default_prog = { 'C:\\Users\\shun\\AppData\\Local\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe' },
