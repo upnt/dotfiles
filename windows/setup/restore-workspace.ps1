392f6288972766a8dffc112cb4007551be9cce2d
upnt <upnt.github@gmail.com>
2024-08-27 23:41:03 +0900

    support chezmoi

--- windows/setup/restore-workspace.ps1
+++
@@ -1,7 +0,0 @@
-$WorkSpaceDir = '~/workspace'
-if ( -not ( Test-Path -Path $WorkSpaceDir ) ) {
-    New-Item $GitSpaceDir -ItemType Directory
-    gh auth login
-    gh repo clone scholor
-    gh repo clone upnt-zenn-contents
-}
