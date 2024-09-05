2a4715a17b64877fb46e1f49dae44a1712e899f4
upnt <upnt.github@gmail.com>
2024-09-05 14:11:03 +0900

    change winget install option

--- run_once_before_install-windows-packages.ps1
+++ run_once_before_install-windows-packages.ps1
@@ -9,15 +9,15 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 
 git clone https://github.com/upnt/nvim-dein "$HOME/AppData/Local/nvim"
 
-winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --id "Neovim.Neovim"
-winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --id "TheBrowserCompany.Arc"
-winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --id "Microsoft.PowerShell"
-winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --id "GitHub.cli"
-winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --id "Git.Git"
-winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --id "7zip.7zip"
-winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --id "lsd-rs.lsd"
-winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --id "sharkdp.bat"
-winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --id "sharkdp.fd"
-winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --id "dandavison.delta"
-winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements "Password Manager SafeInCloud" -s msstore
+winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --disable-interactivity --id "Neovim.Neovim"
+winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --disable-interactivity --id "TheBrowserCompany.Arc"
+winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --disable-interactivity --id "Microsoft.PowerShell"
+winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --disable-interactivity --id "GitHub.cli"
+winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --disable-interactivity --id "Git.Git"
+winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --disable-interactivity --id "7zip.7zip"
+winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --disable-interactivity --id "lsd-rs.lsd"
+winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --disable-interactivity --id "sharkdp.bat"
+winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --disable-interactivity --id "sharkdp.fd"
+winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --disable-interactivity --id "dandavison.delta"
+winget install --allow-reboot --silent --accept-package-agreements --accept-source-agreements --disable-interactivity "Password Manager SafeInCloud" -s msstore
 
