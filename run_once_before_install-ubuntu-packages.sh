1acf9df29bd3385f4e675cb997cb2b91f8ffeed5
upnt <upnt.github@gmail.com>
2024-09-05 10:28:23 +0900

    add fonts

--- run_once_before_install-ubuntu-packages.sh
+++ run_once_before_install-ubuntu-packages.sh
@@ -4,9 +4,9 @@ sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | \
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" |
+	sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 sudo apt update
 sudo apt upgrade -yq
@@ -15,11 +15,11 @@ sudo apt install -y build-essential zlib1g-dev libncurses5-dev \
 	libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev \
 	libcurl4-openssl-dev libxml2-dev libjpeg-dev libonig-dev \
 	libreadline-dev libzip-dev libtidy-dev libmcrypt-dev libxslt-dev \
-	libbz2-dev libsqlite3-dev tk-dev liblzma-dev libyaml-dev ccache \
+	libbz2-dev libsqlite3-dev tk-dev liblzma-dev libyaml-dev ccache zip unzip \
 	autoconf automake openssl gpg dirmngr gawk xdg-utils wget cmake scdoc git \
 	docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
 	zathura xdotool zsh fzf tmux jq zathura
-	
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
@@ -32,22 +32,22 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
 . "$HOME/.asdf/asdf.sh"
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin add lua    https://github.com/Stratus3D/asdf-lua
+asdf plugin add lua https://github.com/Stratus3D/asdf-lua
 asdf plugin add python https://github.com/danhper/asdf-python
-asdf plugin add go     https://github.com/asdf-community/asdf-golang
-asdf plugin add perl   https://github.com/ouest/asdf-perl
-asdf plugin add rust   https://github.com/code-lever/asdf-rust
-asdf plugin add ruby   https://github.com/asdf-vm/asdf-ruby
-asdf plugin add java   https://github.com/halcyon/asdf-java
+asdf plugin add go https://github.com/asdf-community/asdf-golang
+asdf plugin add perl https://github.com/ouest/asdf-perl
+asdf plugin add rust https://github.com/code-lever/asdf-rust
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby
+asdf plugin add java https://github.com/halcyon/asdf-java
 
 asdf install nodejs 20.17.0
-asdf install lua    5.1
+asdf install lua 5.1
 asdf install python 3.10.14
-asdf install go     latest
-asdf install perl 	latest
-asdf install rust 	latest
-asdf install ruby 	latest
-asdf install java 	openjdk-21
+asdf install go latest
+asdf install perl latest
+asdf install rust latest
+asdf install ruby latest
+asdf install java openjdk-21
 
 asdf reshim
 
@@ -75,9 +75,9 @@ sudo update-desktop-database
 sudo mkdir -p /usr/local/share/man/man1
 sudo mkdir -p /usr/local/share/man/man5
 
-scdoc < ~/alacritty/extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
-scdoc < ~/alacritty/extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
-scdoc < ~/alacritty/extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz > /dev/null
-scdoc < ~/alacritty/extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz > /dev/null
+scdoc <~/alacritty/extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz >/dev/null
+scdoc <~/alacritty/extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz >/dev/null
+scdoc <~/alacritty/extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz >/dev/null
+scdoc <~/alacritty/extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz >/dev/null
 
 rm -rf ~/alacritty
---
+++ run_once_ubuntu_fonts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wget https://github.com/yuru7/udev-gothic/releases/download/v2.0.0/UDEVGothic_NF_v2.0.0.zip
+unzip UDEVGothic_NF_v2.0.0.zip
+mkdir -p "$HOME/.local/share/fonts/Unknown Vendor/TrueType/UDEV Gothic NF/"
+mkdir -p "$HOME/.local/share/fonts/Unknown Vendor/TrueType/UDEV Gothic NFLG/"
+mkdir -p "$HOME/.local/share/fonts/Unknown Vendor/TrueType/UDEV Gothic 35NF/"
+mkdir -p "$HOME/.local/share/fonts/Unknown Vendor/TrueType/UDEV Gothic 35NFLG/"
+mv UDEVGothic_NF_v2.0.0/UDEVGothicNF-* "$HOME/.local/share/fonts/Unknown Vendor/TrueType/UDEV Gothic 35NF/"
+mv UDEVGothic_NF_v2.0.0/UDEVGothicNFLG-* "$HOME/.local/share/fonts/Unknown Vendor/TrueType/UDEV Gothic 35NFLG/"
+mv UDEVGothic_NF_v2.0.0/UDEVGothic35NF-* "$HOME/.local/share/fonts/Unknown Vendor/TrueType/UDEV Gothic 35NF/"
+mv UDEVGothic_NF_v2.0.0/UDEVGothic35NFLG-* "$HOME/.local/share/fonts/Unknown Vendor/TrueType/UDEV Gothic 35NFLG/"
+
+rm -r UDEVGothic_NF_v2.0.0
