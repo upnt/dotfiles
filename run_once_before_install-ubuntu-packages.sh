2c10463008267af2f097e7d1c38fa431596dd9f2
upnt <upnt.github@gmail.com>
2024-09-05 14:01:24 +0900

    apt -> apt-get

--- run_once_before_install-ubuntu-packages.sh
+++ run_once_before_install-ubuntu-packages.sh
@@ -10,16 +10,16 @@ echo \
 	sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 # github cli
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y))
+(type -p wget >/dev/null || (sudo apt-get update && sudo apt-get install wget -y))
 sudo mkdir -p -m 755 /etc/apt/keyrings
 wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null
 sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
 
-sudo apt update
-sudo apt upgrade -yq
+sudo apt-get update
+sudo apt-get upgrade -yq
 
-sudo apt install -y build-essential zlib1g-dev libncurses5-dev \
+sudo apt-get install -y build-essential zlib1g-dev libncurses5-dev \
 	libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev \
 	libcurl4-openssl-dev libxml2-dev libjpeg-dev libonig-dev \
 	libreadline-dev libzip-dev libtidy-dev libmcrypt-dev libxslt-dev \
