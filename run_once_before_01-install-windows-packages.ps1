acd0ff6b93e2506c9d5c1e8eeae5f6c161614312
upnt <upnt.github@gmail.com>
2024-09-13 12:44:45 +0900

    fix path

--- run_once_before_01-install-windows-packages.ps1
+++ run_once_before_01-install-windows-packages.ps1
@@ -35,18 +35,21 @@ git clone https://github.com/Shougo/dpp-ext-toml $HOME/.cache/dpp/repos/github.c
 git clone https://github.com/vim-denops/denops.vim $HOME/.cache/dpp/repos/github.com/vim-denops/denops.vim
 
 If ("$(Get-Command rg -ErrorAction SilentlyContinue)" -ne "") {
-    If ("$(Get-ChildItem -File "$HOME\FontTemp" | rg UDEVGothic )" -eq "") {
+    If ("$(Get-ChildItem -File "$HOME\AppData\Local\Microsoft\Windows\Fonts" | rg UDEVGothic )" -eq "") {
+        echo ""
         echo "Downloading UDEVGothic..."
         [string] $filePath = "$(Get-Location)\UDEVGothic_NF_v2.0.0.zip"
         [string] $destPath = "$(Get-Location)\UDEVGothic_NF_v2.0.0"
         [string] $uri = "https://github.com/yuru7/udev-gothic/releases/download/v2.0.0/UDEVGothic_NF_v2.0.0.zip"
         Invoke-WebRequest -Uri $uri -OutFile $filePath -UseBasicParsing
         Expand-Archive -Path $filePath -DestinationPath "$(Get-Location)"
+	mkdir "$HOME\FontTemp"
         foreach ($item in $(Get-ChildItem -File "$destPath")) {
             Move-Item $item.FullName "$HOME\FontTemp"
         }
         Remove-Item $filePath
         Remove-Item -Recurse -Force $destPath
+        echo ""
         echo "Downloaded UDEVGothic to $HOME\FontTemp."
     }
 }
