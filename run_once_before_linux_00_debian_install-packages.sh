245beccd86d7b441e940b9b8783b95588cfcc3b2
upnt <upnt.github@gmail.com>
2024-10-04 19:10:18 +0900

    fix bug

--- run_once_before_linux_00_debian_install-packages.sh
+++ run_once_before_linux_00_debian_install-packages.sh
@@ -16,18 +16,31 @@ wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo t
 sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
 
-sudo apt-get update
-sudo apt-get upgrade -yq
+echo "apt update"
+sudo apt-get update >/dev/null
+echo "apt upgrade"
+sudo apt-get upgrade -yq >/dev/null
 
-sudo apt-get install -y build-essential zlib1g-dev libncurses5-dev \
+echo "install pkgs..."
+sudo apt-get install -y build-essential zlib1g-dev \
 	libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev \
 	libcurl4-openssl-dev libxml2-dev libjpeg-dev libonig-dev \
-	libreadline-dev libzip-dev libtidy-dev libmcrypt-dev libxslt-dev \
+	libreadline-dev libzip-dev libtidy-dev libmcrypt-dev libxslt1-dev \
 	bison gettext libgd-dev libedit-dev libicu-dev libmysqlclient-dev \
 	libpng-dev libpq-dev pkg-config re2c libbz2-dev libsqlite3-dev tk-dev \
-	liblzma-dev libyaml-dev qt5-default fontconfig libwayland-dev libwayland-egl++ wayland-scanner++ \
-	libglu1-mesa-dev libpulse-dev libglib2.0-dev libasound2 libegl1-mesa libnss3 libpci3 libtiff5 \
-	'^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev \
+	liblzma-dev libyaml-dev fontconfig libwayland-dev libwayland-egl-backend-dev wayland-scanner++ \
+	libglu1-mesa-dev libpulse-dev libglib2.0-dev libasound2 libnss3 libpci3 libtiff5 \
+	libwayland-egl-backend-dev libwayland-egl++1 \
+	libwayland-egl1 libwayland-egl1 libxcb-sync-dev \
+	libxcb-damage0-dev libxcb-imdkit-dev libxcb-present-dev \
+	libxcb-xfixes0-dev libxcb-xf86dri0-dev libxcb1-dev libxcb-composite0-dev \
+	libxcb-xinerama0-dev libxcb-xv0-dev libxcb-dri3-dev libxcb-xkb-dev libxcb-image0-dev \
+	libxcb-dri2-0-dev libxcb-util-dev libxcb-shm0-dev libxcb-res0-dev \
+	libxcb-shape0-dev libxcb-render-util0-dev libxcb-xvmc0-dev libxcb-ewmh-dev libxcb-render0-dev \
+	libxcb-cursor-dev libxcb-randr0-dev libxcb-xinput-dev libxcb-dpms0-dev libxcb-record0-dev \
+	libxcb-screensaver0-dev libxcb-keysyms1-dev libxcb-icccm4-dev \
+	libxcb-util0-dev libxcb-glx0-dev libxcb-xrm-dev libxcb-xtest0-dev \
+	libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev \
 	libxcomposite1 libxcursor1 libxi6 libxrandr2 libxtst6 libdbus-1-dev libssl-dev libzstd-dev \
 	ccache zip unzip autoconf automake openssl gpg dirmngr gawk xdg-utils wget cmake scdoc git gh \
 	docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
