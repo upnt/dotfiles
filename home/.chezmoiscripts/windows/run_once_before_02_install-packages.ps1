17f70638117e930e24e90970c3de8be0a22026dc
upnt <upnt.github@gmail.com>
2024-10-19 21:19:19 +0900

    remove repos

--- home/.chezmoiscripts/windows/run_once_before_02_install-packages.ps1
+++ home/.chezmoiscripts/windows/run_once_before_02_install-packages.ps1
@@ -1,10 +1,3 @@
-git clone https://github.com/Shougo/dpp.vim $HOME/.cache/dpp/repos/github.com/Shougo/dpp.vim
-git clone https://github.com/Shougo/dpp-ext-installer $HOME/.cache/dpp/repos/github.com/Shougo/dpp-ext-installer
-git clone https://github.com/Shougo/dpp-protocol-git $HOME/.cache/dpp/repos/github.com/Shougo/dpp-protocol-git
-git clone https://github.com/Shougo/dpp-ext-toml $HOME/.cache/dpp/repos/github.com/Shougo/dpp-ext-toml
-git clone https://github.com/Shougo/dpp-ext-lazy $HOME/.cache/dpp/repos/github.com/Shougo/dpp-ext-lazy
-git clone https://github.com/vim-denops/denops.vim $HOME/.cache/dpp/repos/github.com/vim-denops/denops.vim
-
 If ("$(Get-Command deno -ErrorAction SilentlyContinue)" -eq "") {
 	irm https://deno.land/install.ps1 | iex
 }
