ccd052e7dd2d7cff1822ef96cf1241f19a062029
upnt <upnt.github@gmail.com>
2024-10-23 12:58:23 +0900

    bug fix

--- home/AppData/Local/PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/AppData/Local/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,30 +1,25 @@
+$_HAS_GIT = Get-Command git -ea SilentlyContinue
+
 function prompt {
-    $path = (Get-Location).ToString().Replace((Convert-Path ~), "~").Replace('\', '/')
-    if (gcm git -ea SilentlyContinue) {
-    	$branch = (git branch --show-current)
-    } else {
-	$branch = $null
-    }
-    $conda  = $env:CONDA_PROMPT_MODIFIER
-    $venv  = $env:VIRTUAL_ENV
+    $path = (Get-Location).ToString().Replace((Convert-Path ~), "~")
+    $branch = if ($_HAS_GIT) { git branch --show-current } else { $null }
 
     ## first line
     Write-Host ""
 
     ## second line
     Write-Host "$env:USERNAME" -ForeGroundColor Yellow -NoNewLine
-
     Write-Host " $path" -ForeGroundColor Cyan -NoNewLine
 
-    
-
-    if ($branch -ne $null -and $host.UI.RawUI.WindowSize.Width -gt 70) {
+    if ($branch -and $host.UI.RawUI.WindowSize.Width -gt 70) {
         Write-Host " on " -ForeGroundColor White -NoNewLine
         Write-Host "`u{e725} $branch " -ForeGroundColor Red -NoNewLine
     }
     Write-Host ""
-    if (($conda -ne $null) -And ($venv -eq $null)) {
-    	Write-Host ($conda.SubString(1, $conda.Length - 3)) -ForeGroundColor Magenta -NoNewLine
+    if (($env:CONDA_PROMPT_MODIFIER -ne $null) -And ($env:VIRTUAL_ENV -eq $null)) {
+    	Write-Host (
+            $env:CONDA_PROMPT_MODIFIER.SubString(1, $env:CONDA_PROMPT_MODIFIER.Length - 3)
+        ) -ForeGroundColor Magenta -NoNewLine
     }
     Write-Host "" -ForeGroundColor White -NoNewLine
 
@@ -33,33 +28,22 @@ function prompt {
 
 if ( "$(Get-Command lsd -ErrorAction SilentlyContinue)" -ne "" ) {
 	Set-Alias ls lsd
-	function ll {
-	    ls --long $args
-	}
+	function ll { ls --long $args }
+	function la { ls --all $args }
+}
 	
-	function la {
-	    ls --all $args
-	}
-}
-if ( "$(Get-Command bat -ErrorAction SilentlyContinue)" -ne "" ) {
-	Set-Alias cat bat
-}
-if ( "$(Get-Command rg -ErrorAction SilentlyContinue)" -ne "" ) {
-	Set-Alias grep rg
-}
-if ( "$(Get-Command fd -ErrorAction SilentlyContinue)" -ne "" ) {
-	Set-Alias find fd
-}
+if ( "$(Get-Command bat -ErrorAction SilentlyContinue)" -ne "" ) { Set-Alias cat bat }
+if ( "$(Get-Command rg -ErrorAction SilentlyContinue)" -ne "" ) { Set-Alias grep rg }
+if ( "$(Get-Command fd -ErrorAction SilentlyContinue)" -ne "" ) { Set-Alias find fd }
 
 if ( "$(Get-Command zoxide -ErrorAction SilentlyContinue)" -ne "" ) {
 	Invoke-Expression (& { (zoxide init powershell | Out-String) })
-	Remove-Alias cd
+	Remove-Alias cd -ErrorAction Ignore
 	Set-Alias cd z
 }
 
 Set-PSReadLineKeyHandler -Key 'Ctrl+j' -Function HistorySearchForward
 Set-PSReadLineKeyHandler -Key 'Ctrl+k' -Function HistorySearchBackward
-
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
 . $PSScriptRoot/chezmoi.ps1
