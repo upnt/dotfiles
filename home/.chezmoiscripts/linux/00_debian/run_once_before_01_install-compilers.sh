612cd1d6c305864689a7f91c9ed244b5f0ad98b9
upnt <upnt.github@gmail.com>
2025-03-13 14:51:31 +0900

    remove ssh config

--- home/.chezmoiscripts/linux/00_debian/run_once_before_01_install-compilers.sh
+++ home/.chezmoiscripts/linux/00_debian/run_once_before_01_install-compilers.sh
@@ -105,18 +105,18 @@ if [ ! -d "$LUAENV_ROOT" ]; then
 fi
 
 # java
-export JENV_ROOT="$HOME/.jenv"
-if [ ! -d "$JENV_ROOT" ]; then
-	export PATH="$JENV_ROOT/bin:$PATH"
-
-	git clone https://github.com/jenv/jenv.git "$JENV_ROOT"
-	eval "$(jenv init -)"
-
-	sudo apt-get install -y openjdk-21-jdk
-	jenv add /usr/lib/jvm/java-21-openjdk-amd64
-	jenv global 21
-	jenv enable-plugin export
-fi
+# export JENV_ROOT="$HOME/.jenv"
+# if [ ! -d "$JENV_ROOT" ]; then
+# 	export PATH="$JENV_ROOT/bin:$PATH"
+#
+# 	git clone https://github.com/jenv/jenv.git "$JENV_ROOT"
+# 	eval "$(jenv init -)"
+#
+# 	sudo apt-get install -y openjdk-21-jdk
+# 	jenv add /usr/lib/jvm/java-21-openjdk-amd64
+# 	jenv global 21
+# 	jenv enable-plugin export
+# fi
 
 # julia
 export JULIAUP_ROOT="$HOME/.juliaup"
