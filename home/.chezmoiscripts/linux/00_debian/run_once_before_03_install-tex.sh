e43a886b341f61c4841c8b9dc43d807e48e2d540
upnt <upnt.github@gmail.com>
2025-03-19 16:28:10 +0900

    fix path

--- home/.chezmoiscripts/linux/00_debian/run_once_before_03_install-tex.sh
+++ home/.chezmoiscripts/linux/00_debian/run_once_before_03_install-tex.sh
@@ -3,22 +3,23 @@
 # texlive
 mkdir -p "$HOME/.texlive"
 if [ ! -d "$HOME/.texlive/2022" ]; then
+	_tmp_dir="tmp-tl-2022"
 	wget https://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2022/install-tl-unx.tar.gz
-	mkdir tmp-install-tl && tar xzvf install-tl-unx.tar.gz -C tmp-install-tl --strip-components 1
-	cd tmp-tl-2022 || exit
-	perl ./install-tl --scheme basic --no-interaction --texdir "$HOME/.texlive/2022"
+	mkdir "$_tmp_dir" && tar xzvf install-tl-unx.tar.gz -C "$_tmp_dir" --strip-components 1
+	cd "$_tmp_dir" || exit
+	env TEXDIR="$HOME/.texlive/2022" perl ./install-tl --scheme basic --no-gui --repository https://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2022/tlnet-final/
 	cd - || exit
-	ls
 	rm install-tl-unx.tar.gz
-	rm -rf tmp-tl-2022
+	rm -rf "$_tmp_dir"
 fi
 
 if [ ! -d "$HOME/.texlive/latest" ]; then
+	_tmp_dir="tmp-tl-latest"
 	wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
-	mkdir tmp-install-tl && tar xzvf install-tl-unx.tar.gz -C tmp-install-tl --strip-components 1
-	cd tmp-tl-latest || exit
+	mkdir "$_tmp_dir" && tar xzvf install-tl-unx.tar.gz -C "$_tmp_dir" --strip-components 1
+	cd "$_tmp_dir" || exit
 	perl ./install-tl --scheme basic --no-interaction --texdir "$HOME/.texlive/latest"
 	cd - || exit
 	rm install-tl-unx.tar.gz
-	rm -rf tmp-tl-latest
+	rm -rf "$_tmp_dir"
 fi
