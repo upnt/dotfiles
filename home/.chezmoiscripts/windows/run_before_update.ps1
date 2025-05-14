2009b4baab9996d05dc93ac6410f0fa505ba0a3d
upnt <upnt.github@gmail.com>
2025-05-14 12:19:53 +0900

    fix bug

---
+++ home/.chezmoiscripts/windows/run_before_update.ps1
@@ -0,0 +1,9 @@
+# Self-elevate the script if required
+If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator'))
+{
+	Write-Error "This script must be executed as an administrator"
+	Exit 1
+}
+chcp 65001
+winget update --all
+Exit 0
