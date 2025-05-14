446701d21f30a0425d68c244840aba933d5e23fb
upnt <upnt.github@gmail.com>
2025-05-14 09:09:30 +0900

    fix

--- home/.chezmoiscripts/windows/run_once_before_01_install-requirements.ps1
+++ home/.chezmoiscripts/windows/run_once_before_01_install-requirements.ps1
@@ -1,12 +1,8 @@
 # Self-elevate the script if required
 If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator'))
 {
-	If ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000)
-	{
-		$CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-		Start-Process -Wait -FilePath pwsh.exe -Verb Runas -ArgumentList $CommandLine
-		Exit
-	}
+	Write-Error "This script must be executed as an administrator"
+	Exit 1
 }
 chcp 65001
 Write-Output ""
