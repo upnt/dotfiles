be77c9b4c110dc886810da006887aa09ded2175e
upnt <upnt.github@gmail.com>
2025-05-14 12:14:09 +0900

    fix update script

--- home/.chezmoiscripts/linux/00_debian/run_z0_before_update.sh
+++ home/.chezmoiscripts/linux/00_debian/run_z0_before_update.sh
@@ -5,4 +5,38 @@ sudo apt-get update
 sudo apt-get full-upgrade -y
 sudo apt-get autoremove
 
-cd ~/.fzf && git pull && ./install --bin && cd -
+if [ -d "$HOME/.tmux" ]; then
+  cd ~/.tmux/bin || exit 1
+  git fetch
+  
+  LOCAL=$(git rev-parse HEAD)
+  REMOTE=$(git rev-parse @{u})
+
+  if [ "$LOCAL" != "$REMOTE" ]; then
+    echo "update tmux"
+    git pull
+    sh autogen.sh
+    ./configure && make
+  else
+    echo "tmux is already up to date"
+  fi
+  
+  cd - || exit 1
+fi
+
+if [ -d "$HOME/.fzf" ]; then
+  cd ~/.fzf || exit 1
+  git fetch
+  
+  LOCAL=$(git rev-parse HEAD)
+  REMOTE=$(git rev-parse @{u})
+
+  if [ "$LOCAL" != "$REMOTE" ]; then
+    echo "update fzf"
+    git pull && ./install --bin
+  else
+    echo "fzf is already up to date"
+  fi
+  
+  cd - || exit 1
+fi
