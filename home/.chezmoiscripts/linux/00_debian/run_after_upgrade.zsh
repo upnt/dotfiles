aa8c8c5c15bb24974e357a5e42eeb282ab6428fb
upnt <upnt.github@gmail.com>
2025-05-14 10:24:06 +0900

    fix bug

--- home/.chezmoiscripts/linux/00_debian/run_after_upgrade.zsh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-
-cd ~/.fzf && git pull && ./install --bin
-cd - || return 0
--- home/.chezmoiscripts/linux/00_debian/run_z0_before_update.sh
+++ home/.chezmoiscripts/linux/00_debian/run_z0_before_update.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
 echo "update packages"
 sudo apt-get update
 sudo apt-get full-upgrade -y
 sudo apt-get autoremove
+
+cd ~/.fzf && git pull && ./install --bin
+cd - || return 0
