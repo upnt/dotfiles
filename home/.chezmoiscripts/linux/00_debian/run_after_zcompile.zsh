c58bf2804e958e9911654fcdc2a65b408e2352c3
upnt <upnt.github@gmail.com>
2025-08-12 19:34:38 +0900

    zsh -> bash

--- home/.chezmoiscripts/linux/00_debian/run_after_zcompile.zsh
+++ home/.chezmoiscripts/linux/00_debian/run_after_zcompile.zsh
@@ -1,4 +1,7 @@
-#!/usr/bin/zsh
+#!/usr/bin/bash
+
+command -v zsh >/dev/null 2>&1 || exit 0
+exec zsh "$0" "$@"
 
 for file in ~/.local/zsh/.* ~/.local/zsh/.zsh_functions/* ~/.local/zsh_local/.*; do
   if [[ -f "$file" && "$file" != *.zwc && "$file" != *.zsh_history ]]; then
