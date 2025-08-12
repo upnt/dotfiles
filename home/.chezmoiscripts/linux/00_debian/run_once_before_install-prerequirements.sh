f3b0bbdf83325bc685b1c92b5e4ccf2c6e5a36d1
upnt <upnt.github@gmail.com>
2025-08-13 04:28:11 +0900

    update log

--- home/.chezmoiscripts/linux/00_debian/run_once_before_install-prerequirements.sh
+++ home/.chezmoiscripts/linux/00_debian/run_once_before_install-prerequirements.sh
@@ -34,7 +34,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubc
 # Install packages
 echo "Installing packages"
 sudo apt-get install -yqq build-essential zlib1g-dev \
-	libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev \
+	libgdbm-dev libnss3-dev libssl-dev libffi-dev \
 	libcurl4-openssl-dev libxml2-dev libjpeg-dev libonig-dev \
 	libreadline-dev libzip-dev libtidy-dev libmcrypt-dev libxslt1-dev \
 	bison gettext libgd-dev libedit-dev libicu-dev libmysqlclient-dev \
--- home/.chezmoiscripts/linux/00_debian/run_z2_before_golang.sh
+++ home/.chezmoiscripts/linux/00_debian/run_z2_before_golang.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-echo "Installing golang"
+LOG="/tmp/install_go.log"
+
+run() {
+	local msg="$1"
+	shift
+	echo ".${msg}"
+	"$@" >>"$LOG" 2>&1
+}
+
+trap 'echo "✖ エラー発生。ログ: $LOG"; tail -n 80 "$LOG"' ERR
 
 # golang (No goenv for backward compatibility)
 if [ ! -d /opt/go-1.12.3 ]; then
 	export PATH="/opt/go-1.12.3/bin:$PATH"
 
 	cd /tmp || exit 1
-	wget https://go.dev/dl/go1.23.3.linux-amd64.tar.gz
-	tar -xzf go1.23.3.linux-amd64.tar.gz
+	run "Downloading Go 1.12.3" \
+		wget https://go.dev/dl/go1.23.3.linux-amd64.tar.gz
+	run "Extracting Go 1.12.3" \
+		tar -xzf go1.23.3.linux-amd64.tar.gz
 	sudo mv go /opt/go-1.12.3
 	rm /tmp/go1.23.3.linux-amd64.tar.gz
 
-	go install github.com/dundee/gdu/v5/cmd/gdu@latest
-	go install github.com/jesseduffield/lazygit@latest
-	go install github.com/x-motemen/ghq@latest
+	run "Installing gdu" \
+		go install github.com/dundee/gdu/v5/cmd/gdu@latest
+	run "Installing lazygit" \
+		go install github.com/jesseduffield/lazygit@latest
+	run "Installing ghq" \
+		go install github.com/x-motemen/ghq@latest
 fi
