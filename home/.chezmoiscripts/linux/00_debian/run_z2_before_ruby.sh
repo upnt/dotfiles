103dec291d7c72af8033e8dd87d0cb4e224d69c9
upnt <upnt.github@gmail.com>
2025-08-13 14:14:48 +0900

    compact

--- home/.chezmoiscripts/linux/00_debian/run_z2_before_ruby.sh
+++ home/.chezmoiscripts/linux/00_debian/run_z2_before_ruby.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-echo "Installing ruby"
+LOG="/tmp/install_cpp.log"
+
+run() {
+	local msg="$1"
+	shift
+	echo ".${msg}"
+	"$@" >>"$LOG" 2>&1
+}
+
+trap 'echo "✖ エラー発生。ログ: $LOG"; tail -n 80 "$LOG"' ERR
 
 # ruby
 export RBENV_ROOT="$HOME/.rbenv"
 if [ ! -d "$RBENV_ROOT" ]; then
 	export PATH="$RBENV_ROOT/bin:$PATH"
 
-	git clone https://github.com/rbenv/rbenv.git "$RBENV_ROOT"
-	git clone https://github.com/rbenv/ruby-build.git "$RBENV_ROOT/plugins/ruby-build"
-	git clone https://github.com/rkh/rbenv-update.git "$RBENV_ROOT/plugins/rbenv-update"
+	run "Installing rbenv" \
+		git clone https://github.com/rbenv/rbenv.git "$RBENV_ROOT"
+	run "Installing ruby-build" \
+		git clone https://github.com/rbenv/ruby-build.git "$RBENV_ROOT/plugins/ruby-build"
+	run "Installing rbenv-update" \
+		git clone https://github.com/rkh/rbenv-update.git "$RBENV_ROOT/plugins/rbenv-update"
 	eval "$(rbenv init - zsh)"
 
-	rbenv install 3.3.4
+	run "Installing ruby 3.3.4" \
+		rbenv install 3.3.4
 	rbenv global 3.3.4
 
-	gem install neovim
+	run "Installing neovim plugin on ruby 3.3.4" \
+		gem install neovim
 fi
