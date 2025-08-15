78d41fbb8db66502949972920ea9e13ca0a71b48
upnt <upnt.github@gmail.com>
2025-08-15 13:23:01 +0900

    fix github cli installation

--- home/.chezmoiscripts/linux/00_debian/run_once_before_install-prerequirements.sh
+++ home/.chezmoiscripts/linux/00_debian/run_once_before_install-prerequirements.sh
@@ -33,11 +33,13 @@ echo \
 	sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 # Install github cli
-sudo mkdir -p -m 0755 /etc/apt/keyrings
-run "Installing github-cli keyring" \
-	wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null
-sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) &&
+	sudo mkdir -p -m 755 /etc/apt/keyrings &&
+	out=$(mktemp) && wget -nv -O"$out" https://cli.github.com/packages/githubcli-archive-keyring.gpg &&
+	cat "$out" | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
+	sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg &&
+	sudo mkdir -p -m 755 /etc/apt/sources.list.d &&
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
 
 # Reupdate packages.
 run "Reupdating packages" \
