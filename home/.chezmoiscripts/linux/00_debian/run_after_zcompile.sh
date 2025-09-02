83557577912791c1c182527cf2f45a852e28b8ad
upnt <upnt.github@gmail.com>
2025-09-02 20:39:18 +0900

    bug fix

--- home/.chezmoiscripts/linux/00_debian/run_after_zcompile.sh
+++ home/.chezmoiscripts/linux/00_debian/run_after_zcompile.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-command -v zsh >/dev/null 2>&1 || exit 0
-exec zsh "$0" "$@"
-
 for file in ~/.local/zsh/.* ~/.local/zsh/.zsh_functions/* ~/.local/zsh_local/.*; do
   if [[ -f "$file" && "$file" != *.zwc && "$file" != *.zsh_history ]]; then
     echo "zcompile $file"
-    zcompile "$file"
+    zsh -c "zcompile $file"
   fi
 done
