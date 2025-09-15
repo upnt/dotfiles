c5561bb00c6cb888725811291ca0818eb7224b0a
upnt <upnt.github@gmail.com>
2025-09-15 09:35:40 +0900

    remove docker path

--- home/.chezmoiscripts/linux/01_wsl/run_once_before_00_setup-path.sh
+++ home/.chezmoiscripts/linux/01_wsl/run_once_before_00_setup-path.sh
@@ -8,11 +8,3 @@ fi
 if [ -z "$(which clip.exe)" ]; then
   sudo ln -s /mnt/c/Windows/system32/clip.exe /usr/local/bin/clip.exe
 fi
-
-if [ -z "$(which docker)" ]; then
-  sudo ln -s "/mnt/c/Program Files/Docker/Docker/resources/bin/docker.exe" /usr/local/bin/docker
-fi
-
-if [ -z "$(which docker.exe)" ]; then
-  sudo ln -s "/mnt/c/Program Files/Docker/Docker/resources/bin/docker.exe" /usr/local/bin/docker.exe
-fi
