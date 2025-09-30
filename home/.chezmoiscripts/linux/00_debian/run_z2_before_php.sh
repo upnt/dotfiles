e9dd5ae4d3a15df3864b9cebabe6064f9efbeb0b
upnt <upnt.github@gmail.com>
2025-09-30 09:15:38 +0900

    fix composer hash

--- home/.chezmoiscripts/linux/00_debian/run_z2_before_php.sh
+++ home/.chezmoiscripts/linux/00_debian/run_z2_before_php.sh
@@ -16,12 +16,13 @@ trap 'echo "✖ エラー発生。ログ: $LOG"; tail -n 80 "$LOG"' ERR
 if [ -z "$(which composer)" ]; then
 	sudo apt-get update
 	sudo apt-get install -yqq php-common php-mbstring libapache2-mod-php php-cli php-curl php-xml
+
 	run "Download Composer installer" \
 		php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 	run "Verify Composer installer" \
-		php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+		php -r "if (hash_file('sha384', 'composer-setup.php') === 'ed0feb545ba87161262f2d45a633e34f591ebb3381f2e0063c345ebea4d228dd0043083717770234ec00c5a9f9593792') { echo 'Installer verified'.PHP_EOL; } else { echo 'Installer corrupt'.PHP_EOL; unlink('composer-setup.php'); exit(1); }"
 	run "Install Composer" \
-		sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+		php composer-setup.php
 	run "Cleanup Composer installer" \
 		php -r "unlink('composer-setup.php');"
 fi
